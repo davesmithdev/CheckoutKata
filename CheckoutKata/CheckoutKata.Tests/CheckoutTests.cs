182b6e1c62852ba044742fd5352feb8ef0d3f931
davesmithdev <davesmithdev1@gmail.com>
2017-01-12 17:17:28 +0000

    Refactored CheckoutTests.cs. Tests still pass.
    Removed duplication in tests where each method was responsible for
    instantiating the Checkout class. Created a Setup method that
    instantiates the Checkout for each test. Tests still pass.

--- CheckoutKata/CheckoutKata.Tests/CheckoutTests.cs
+++ CheckoutKata/CheckoutKata.Tests/CheckoutTests.cs
@@ -5,25 +5,29 @@ namespace CheckoutKata.Tests
     [TestFixture]
     public class CheckoutTests
     {
+        private ICheckout _checkout;
+
+        [SetUp]
+        public void Setup()
+        {
+            _checkout = new Checkout();
+        }
+
         [Test]
         public void GivenIScanItemA_WhenICallGetTotalPrice_TheTotalPriceIs50()
         {
-            ICheckout checkout = new Checkout();
+            _checkout.Scan("A");
 
-            checkout.Scan("A");
-
-            Assert.That(checkout.GetTotalPrice(), Is.EqualTo(50));
+            Assert.That(_checkout.GetTotalPrice(), Is.EqualTo(50));
         }
 
         [Test]
         public void GivenIScanItemATwoTimes_WhenICallGetTotalPrice_TheTotalPriceIs100()
         {
-            ICheckout checkout = new Checkout();
+            _checkout.Scan("A");
+            _checkout.Scan("A");
 
-            checkout.Scan("A");
-            checkout.Scan("A");
-
-            Assert.That(checkout.GetTotalPrice(), Is.EqualTo(100));
+            Assert.That(_checkout.GetTotalPrice(), Is.EqualTo(100));
         }
     }
 }
