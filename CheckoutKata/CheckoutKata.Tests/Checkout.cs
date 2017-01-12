b3f4c084de44fa2a883302a727ca65a950dd491e
David Smith <davesmithdev1@gmail.com>
2017-01-12 22:18:38 +0000

    Seventh test passes.
    Fixed 7th test. Added data structure to store the discount details for
    each product. Also added logic to calculate the total discount price and
    deduct this from the total cost of scanned items.

--- CheckoutKata/CheckoutKata.Tests/Checkout.cs
+++ CheckoutKata/CheckoutKata.Tests/Checkout.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -13,6 +14,11 @@ namespace CheckoutKata.Tests
             {"C", 20},
             {"D", 15}
         };
+        private readonly Dictionary<string, Tuple<int, int>> _priceRules = new Dictionary<string, Tuple<int, int>>()
+        {
+            {"A", new Tuple<int, int>(3, 130)},
+            {"B", new Tuple<int, int>(2, 45)}
+        }; 
 
         public void Scan(string item)
         {
@@ -23,8 +29,11 @@ namespace CheckoutKata.Tests
         {
             var totalPrice = _scannedItems.Sum(item => _prices[item]);
 
-            if (_scannedItems.Count(i => i == "A") == 3)
-                totalPrice -= 20;
+            foreach (var priceRule in _priceRules)
+            {
+                var numberOfDiscountsToApply = _scannedItems.Count(x => x == priceRule.Key) / priceRule.Value.Item1;
+                var discountPrice = (_priceRules[priceRule.Key].Item1 *  _prices[priceRule.Key]) - priceRule.Value.Item2;
+            }
 
             return totalPrice;
         }
