b0384a62e37b5010679146db9ba95899d06831a3
davesmithdev <davesmithdev1@gmail.com>
2017-01-12 16:58:05 +0000

    First test. Scan Item 'A'. Test fails.
    Added first test to scan item 'A' and assert the correct total price is
    calculated when the GetTotalPrice method is called. Test fails.

---
+++ CheckoutKata/CheckoutKata.Tests/CheckoutTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace CheckoutKata.Tests
+{
+    [TestFixture]
+    public class CheckoutTests
+    {
+        [Test]
+        public void GivenIScanItemA_WhenICallGetTotalPrice_TheTotalPriceIs50()
+        {
+            ICheckout checkout = new Checkout();
+
+            checkout.Scan("A");
+
+            Assert.That(checkout.GetTotalPrice(), Is.EqualTo(50));
+        }
+    }
+
+    public class Checkout : ICheckout
+    {
+        public void Scan(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface ICheckout
+    {
+        void Scan(string item);
+        int GetTotalPrice();
+    }
+}
