6c4aefe2ccabe66f69e5c2ac8d7a44f9be10ac03
David Smith <davesmithdev1@gmail.com>
2017-01-12 23:45:36 +0000

    missed file from last commit

--- CheckoutKata/CheckoutKata.Tests/Checkout.cs
+++ CheckoutKata/CheckoutKata.Tests/Checkout.cs
@@ -7,18 +7,17 @@ namespace CheckoutKata.Tests
     public class Checkout : ICheckout
     {        
         private readonly List<string> _scannedItems = new List<string>();
-        private readonly Dictionary<string, int> _prices = new Dictionary<string, int>()
-        {
-            {"A", 50},
-            {"B", 30},
-            {"C", 20},
-            {"D", 15}
-        };
+        private IProductService _productService { get; set; }       
         private readonly Dictionary<string, Tuple<int, int>> _priceRules = new Dictionary<string, Tuple<int, int>>()
         {
             {"A", new Tuple<int, int>(3, 130)},
             {"B", new Tuple<int, int>(2, 45)}
-        }; 
+        };
+
+        public Checkout(IProductService productService)
+        {
+            _productService = productService;
+        }
 
         public void Scan(string item)
         {
@@ -27,21 +26,23 @@ namespace CheckoutKata.Tests
 
         public int GetTotalPrice()
         {
-            var totalPrice = _scannedItems.Sum(item => _prices[item]);
+            var prices = _productService.GetProducts();
+
+            var totalPrice = _scannedItems.Sum(item => prices[item]);
 
             foreach (var priceRule in _priceRules)
             {
                 var numberOfDiscountToApply = NumberOfDiscountsToApply(priceRule);
-                var totalDiscountPrice = SingleDiscountAmount(priceRule) * numberOfDiscountToApply;
+                var totalDiscountPrice = SingleDiscountAmount(priceRule, prices) * numberOfDiscountToApply;
                 totalPrice -= totalDiscountPrice;
             }
 
             return totalPrice;
         }
 
-        private int SingleDiscountAmount(KeyValuePair<string, Tuple<int, int>> priceRule)
+        private int SingleDiscountAmount(KeyValuePair<string, Tuple<int, int>> priceRule, Dictionary<string, int> prices)
         {
-            return (ForHowMany(priceRule) * ItemPrice(priceRule) - ForHowMuch(priceRule));
+            return (ForHowMany(priceRule) * ItemPrice(priceRule, prices) - ForHowMuch(priceRule));
         }
 
         private int NumberOfDiscountsToApply(KeyValuePair<string, Tuple<int, int>> priceRule)
@@ -54,9 +55,9 @@ namespace CheckoutKata.Tests
             return priceRule.Value.Item2;
         }
 
-        private int ItemPrice(KeyValuePair<string, Tuple<int, int>> priceRule)
+        private int ItemPrice(KeyValuePair<string, Tuple<int, int>> priceRule, Dictionary<string, int> prices)
         {
-            return _prices[priceRule.Key];
+            return prices[priceRule.Key];
         }
 
         private int ForHowMany(KeyValuePair<string, Tuple<int, int>> priceRule)
