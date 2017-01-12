d91556a4401ad001f6a232040b4311a820631f6e
davesmithdev <davesmithdev1@gmail.com>
2017-01-12 17:31:03 +0000

    Third test passes.
    Fixed third test. Add variable of type List<string> to store scanned
    items when they are scanned. Loop through each scanned item when the
    GetTotalPrice method is called and increment the total price
    accordingly.

--- CheckoutKata/CheckoutKata.Tests/Checkout.cs
+++ CheckoutKata/CheckoutKata.Tests/Checkout.cs
@@ -1,16 +1,24 @@
+using System.Collections.Generic;
+
 namespace CheckoutKata.Tests
 {
     public class Checkout : ICheckout
     {
         private int _totalPrice;
+        private List<string> _scannedItems = new List<string>();
 
         public void Scan(string item)
         {
-            _totalPrice += 50;
+            _scannedItems.Add(item);
         }
 
         public int GetTotalPrice()
         {
+            foreach (var item in _scannedItems)
+            {
+                _totalPrice += (item == "A") ? 50 : 30;
+            }
+
             return _totalPrice;
         }
     }
